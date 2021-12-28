8efdc46480a90e1d386e28f9dfb837ebea4e06d9
matiasjgelpi <matiasjgelpi@gmail.com>
2021-12-28 12:06:26 -0300

    seccion 4 home

---
+++ src/components/cobertura/Cobertura.js
@@ -0,0 +1,47 @@
+import { Row, Col } from "react-bootstrap";
+
+export const Cobertura = () => {
+  return (
+    <>
+      <Row className="home cobertura">
+        <Col id="" className="" lg={"6"}>
+          <div className="rectangulo-amarillo"></div>
+          <h3>COBERTURA</h3>
+          <h4>
+            SEGUÍ EN EVENTO EN LO MEDIOS Y
+            <br /> REDES SOCIALES
+          </h4>
+        </Col>
+        <Row className="d-flex justify-content-between">
+          <Col lg={1} className="medios-cobertura-container">
+            <div className="iconos" src="./antena.png" id="antena" alt="" />
+            <p className="titulo-iconos ">Radio y TV</p>
+            <p className="texto-iconos">Max Maxima 95.5 <br/>FM Tostado Cable</p>
+          </Col>
+          <Col lg={1} className="medios-cobertura-container">
+            <div className="iconos" src="./instagram.png" id="instagram" alt=""/>
+            <p className="titulo-iconos">Instagram</p>
+            <p className="texto-iconos">
+              @atleticotostado @kimika @jazminarmando @ignacio.marmol @berlin
+            </p>
+          </Col>
+          <Col lg={1} className="medios-cobertura-container">
+            <div className="iconos" src="./tiktok.png" id="tiktok" alt="" />
+            <p className="titulo-iconos">TikTok</p>
+            <p className="texto-iconos">@jazminarmando</p>
+          </Col>
+          <Col lg={1} className="medios-cobertura-container">
+            <div className="iconos" src="./facebook.png" id="facebook" alt="" />
+            <p className="titulo-iconos">Facebook</p>
+            <p className="texto-iconos">@atleticotostado1989</p>
+          </Col>
+          <Col lg={1} className="medios-cobertura-container">
+            <div className="iconos" src="./twitch.png" id="twitch" alt="" />
+            <p className="titulo-iconos">Twitch</p>
+            <p className="texto-iconos">@el_ignaaa</p>
+          </Col>
+        </Row>
+      </Row>
+    </>
+  );
+};
