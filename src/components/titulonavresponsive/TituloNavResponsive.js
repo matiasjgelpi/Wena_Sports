7d7c6a574718a60bccab4875ea2f362b0b481767
matiasjgelpi <matiasjgelpi@gmail.com>
2022-01-05 16:24:07 -0300

    nav responsive v1.2

--- src/components/titulonavresponsive/TituloNavResponsive.js
+++ src/components/titulonavresponsive/TituloNavResponsive.js
@@ -1,11 +1,7 @@
-// import { Navbar, Container, Nav } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+
 
 
 export const TituloNavResponsive = () => {
-
-    const { cat } = useParams();
-    console.log(cat)
 
   return (
     <div className="titulo-responsive-nav">
