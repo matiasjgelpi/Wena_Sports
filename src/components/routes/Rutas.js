4be85d40f6673843630c4fedaf4db3e49c762643
matiasjgelpi <matiasjgelpi@gmail.com>
2022-01-15 12:27:39 -0300

    home  responsive

--- src/components/routes/Rutas.js
+++ src/components/routes/Rutas.js
@@ -15,7 +15,7 @@ export default function Rutas() {
           <Route path="/category/:cat" exact element={<SectionContainer/>} />
         </Routes>
       </Router>{" "}
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
